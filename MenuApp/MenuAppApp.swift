e09c6ffb58e42b01fff03f8f7c2d55e6a828cc89
Dave McCollough <davemccollough@gmail.com>
2024-09-30 12:32:30 -0600

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Dave McCollough on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
